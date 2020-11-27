750b424331141de7ada828344bbaee6019d9eb33
mestre99 <48585228+mestresas99@users.noreply.github.com>
2020-11-27 14:27:52 +0100

    Add files via upload

---
+++ Arqui2020-main/cmake-build-debug/CMakeFiles/Arqui2020.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Arqui2020"
+  "Arqui2020.pdb"
+  "CMakeFiles/Arqui2020.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Arqui2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
