652376d732000dc248b7adcafe7bffba6ec15047
Roberto <robertovaquerici@gmail.com>
2020-11-29 22:09:43 +0100

    Added compiler flags to CMakeLists.txt, cleaned unused things

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/davidryt/Desktop/Arqui2020
+# Install script for directory: /home/rober/uni/arch/Arqui2020
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/davidryt/Desktop/Arqui2020/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/rober/uni/arch/Arqui2020/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
